405518c398716c23d557d23f022dbecff77d24af
gabileitao <marialeitaoautora@gmail.com>
2023-01-14 00:48:47 -0300

    criacao do projeto

---
+++ hotelreception-project/hotelreception-project/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace hotelreception_project {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
